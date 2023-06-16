62cd974d4192e3bc7f727ed10a4b1d016fcdb8a0
Elsayed Mustafa <engelsayedmustafa123@gmail.com>
2023-06-16 16:41:18 +0300

    update solutions

--- Algorithms/Greedy/2-Sheet/Problem3 Highest Increase Pair/solution.js
+++ Algorithms/Greedy/2-Sheet/Problem3 Highest Increase Pair/solution.js
@@ -1,20 +1,24 @@
 function highestIncreasePair(arr) {
-    current = arr[0]
-    max = NegativeInfinity
+    let current = arr[0]; // Initialize current element to the first element of the array
+    let max = -Infinity; // Initialize max difference to negative infinity
 
-    for (var i = 1; i < arr.length; i++) {
-        {
-            diff = arr[i] - current
+    for (let i = 1; i < arr.length; i++) {
+        let diff = arr[i] - current; // Calculate the difference between the current element and the current
 
-            if (arr[i] < current) {
-                current = arr[i]
-                continue
-            }
+        if (arr[i] < current) {
+            current = arr[i]; // Update the current element if a smaller element is found
+            continue;
+        }
 
-            if (diff > max) {
-                max = diff
-            }
+        if (diff > max) {
+            max = diff; // Update the max difference if a larger difference is found
         }
     }
-    return max
+
+    return max; // Return the maximum difference
 }
+
+// Example usage:
+const inputArray = [1, 4, 7, 2, 9, 3];
+const highestIncrease = highestIncreasePair(inputArray);
+console.log('Highest increase:', highestIncrease);
